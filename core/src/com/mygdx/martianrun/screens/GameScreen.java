91cc2df7fe1af977d0a10d649828d052cb0156f2
Michael Kemp <mkemp@accu-time.com>
2017-11-07 12:22:39 -0500

    Add Ground (just debug lines for now)

--- core/src/com/mygdx/martianrun/screens/GameScreen.java
+++ core/src/com/mygdx/martianrun/screens/GameScreen.java
@@ -1,6 +1,9 @@
 package com.mygdx.martianrun.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.mygdx.martianrun.stages.GameStage;
 
 /**
  * Created by mkemp on 11/7/17.
@@ -8,9 +11,20 @@ import com.badlogic.gdx.Screen;
 
 public class GameScreen implements Screen {
 
+    private GameStage stage;
+
+    public GameScreen() {
+        stage = new GameStage();
+    }
+
     @Override
     public void render(float delta) {
+        // Clear the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Update the stage
+        stage.draw();
+        stage.act(delta);
     }
 
     @Override
--- core/src/com/mygdx/martianrun/utils/Constants.java
+++ core/src/com/mygdx/martianrun/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.mygdx.martianrun.utils;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by mkemp on 11/7/17.
  */
@@ -9,4 +11,11 @@ public class Constants {
     public static final int APP_WIDTH = 800;
     public static final int APP_HEIGHT = 480;
 
+    public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
+
+    public static final float GROUND_X = 0;
+    public static final float GROUND_Y = 0;
+    public static final float GROUND_WIDTH = 25f;
+    public static final float GROUND_HEIGHT = 2f;
+    public static final float GROUND_DENSITY = 0f;
 }
