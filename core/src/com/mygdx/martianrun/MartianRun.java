fddacd0907426dbbfeec69e7468c65defca0c9c6
Michael Kemp <mkemp@accu-time.com>
2017-11-07 11:13:35 -0500

    Initial Commit

---
+++ core/src/com/mygdx/martianrun/MartianRun.java
@@ -0,0 +1,16 @@
+package com.mygdx.martianrun;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class MartianRun extends ApplicationAdapter {
+
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+}
---
+++ desktop/src/com/mygdx/martianrun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.martianrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.martianrun.MartianRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MartianRun(), config);
+	}
+}
